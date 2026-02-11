08749bdbea2f33508eb2574e132755d249c36ce5
Niruta <karkeeneeruta@gmail.com>
2026-02-11 21:07:34 +0545

    not important

---
+++ app/Http/Controllers/Api/LoadController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoadController extends Controller
+{
+    public function index()
+    {
+        return Load::all();
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'pickup_location' => 'required',
+            'delivery_location' => 'required',
+            'business_id' => 'required|exists:businesses,id'
+        ]);
+
+        $load = Load::create($validated);
+
+        return response()->json($load, 201);
+    }
+}
